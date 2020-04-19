2af209b9ea0e3e13f368e705b336170b7c5662de
Dungnv <ksgt.nvdung1@gmail.com>
2020-04-19 16:44:26 +0700

    Improve cat eat method

--- Cat.js
+++ Cat.js
@@ -1,10 +1,19 @@
+var Mouse = require('./Mouse');
+
 function Cat()
 {
     this.stomach=[];
 
 }
-Cat.prototype.eat= function (mouse)
+Cat.prototype.eat= function (animal)
 {
-    this.stomach.push(mouse);
+    if(animal instanceof Mouse)
+    {
+        this.stomach.push(animal);
+
+    }else
+    {
+        throw new Error('Cat can only eat mouse!');      
+    }    
 }
 module.exports=Cat;
